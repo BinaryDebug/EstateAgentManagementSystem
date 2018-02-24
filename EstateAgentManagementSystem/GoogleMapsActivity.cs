62ebb2576a0a0a7f1b66e5323bc6b9dbd82f4775
Thomas Hanson <binarydebugging@gmail.com>
2018-02-24 23:25:28 +0000

    Small changes to Google Maps to be more accurate, still changes to be made

--- EstateAgentManagementSystem/GoogleMapsActivity.cs
+++ EstateAgentManagementSystem/GoogleMapsActivity.cs
@@ -19,25 +19,48 @@ namespace EstateAgentManagementSystem
     [Activity(Label = "Editor", Theme = "@style/NotesEditTheme", Icon = "@drawable/ic_launcher")]
     public class GoogleMapsActivity : Activity, IOnMapReadyCallback
     {
+        private GoogleMap GMap;
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.GoogleMapsView);
-            MapFragment mapFragment = (MapFragment)FragmentManager.FindFragmentById(Resource.Id.map);
-            mapFragment.GetMapAsync(this);
+
+            if (GMap == null)
+            {
+                FragmentManager.FindFragmentById<MapFragment>(Resource.Id.map).GetMapAsync(this);
+            }
 
         }
 
         public void OnMapReady(GoogleMap googleMap)
         {
-            MarkerOptions markerOptions = new MarkerOptions();
-            markerOptions.SetPosition(new LatLng(16.03, 108));
-            markerOptions.SetTitle("My Position");
-            googleMap.AddMarker(markerOptions);
+//            MarkerOptions markerOptions = new MarkerOptions();
+//            markerOptions.SetPosition(new LatLng(16.03, 108));
+//            markerOptions.SetTitle("My Position");
+//            googleMap.AddMarker(markerOptions);
+//
+//            googleMap.TrafficEnabled = true;
+//            googleMap.MapType = GoogleMap.MapTypeHybrid;
+//            CameraUpdate cam = CameraUpdateFactory.NewLatLngZoom(markerOptions.Position, 15);
+//
+//            googleMap.MoveCamera(cam);
+//            googleMap.UiSettings.ZoomControlsEnabled = true;
+//            googleMap.UiSettings.CompassEnabled = true;
+//            googleMap.MoveCamera(CameraUpdateFactory.ZoomIn());
 
-            googleMap.UiSettings.ZoomControlsEnabled = true;
-            googleMap.UiSettings.CompassEnabled = true;
-            googleMap.MoveCamera(CameraUpdateFactory.ZoomIn());
+            this.GMap = googleMap;
+
+            GMap.UiSettings.ZoomControlsEnabled = true;
+
+            LatLng latLng = new LatLng(Convert.ToDouble(13.0291), Convert.ToDouble(80.2083));
+            CameraUpdate camera = CameraUpdateFactory.NewLatLngZoom(latLng, 15);
+            GMap.MoveCamera(camera);
+
+            MarkerOptions options = new MarkerOptions()
+                .SetPosition(latLng)
+                .SetTitle("A title");
+
+            GMap.AddMarker(options);
         }
 
     }
